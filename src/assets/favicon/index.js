09c84356e49491f03c5ad3a59d7b305f9fa98190
6okuJins <d.ma@mail.utoronto.ca>
2022-08-19 19:19:15 -0400

    Update meta

--- src/assets/favicon/index.js
+++
@@ -1,2 +0,0 @@
-export { default as favicon } from './favicon.ico';
-export { default as appleTouchIcon } from './apple-touch-icon.png';
--- src/index.js
+++ src/index.js
@@ -1,19 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import Helmet from 'react-helmet';
-import {favicon, appleTouchIcon } from './assets/favicon';
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Helmet>
-      <meta charSet="utf-8" />
-      <title>Dominic's Portfolio</title>
-      <link rel="icon" type="image/x-icon" href={favicon}></link>
-      <link rel="apple-touch-icon" sizes="180x180" href={appleTouchIcon}></link>
-
-    </Helmet>
     <App />
   </React.StrictMode>
 );
