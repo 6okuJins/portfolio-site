d0b03f00e0c641d441c88faa42c0edd939e5d945
6okuJins <d.ma@mail.utoronto.ca>
2022-08-24 06:05:31 -0400

    Add title SVG and give path color gradient and animate morphing circle

--- src/assets/svg/index.js
+++ src/assets/svg/index.js
@@ -1,3 +1,2 @@
-export { ReactComponent as Circle } from './circle.svg';
-export { ReactComponent as Rectangle } from './Rectangle.svg';
-export { ReactComponent as TitleSVG } from './Group 1.svg';
+export { ReactComponent as DominicSVG } from './Dominic.svg';
+export { ReactComponent as MorphingCircleSVG } from './group1.svg';
--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -1,14 +1,46 @@
 import {useEffect} from 'react';
-import {  TitleSVG } from '../../assets/svg';
-import { TitleStyle } from '../../styles'
+import { MorphingCircleSVG , DominicSVG } from '../../assets/svg';
+import { TitleStyle } from '../../styles';
 import KUTE from 'kute.js';
+import {GradientPath} from 'gradient-path';
+
 const Title = () => {
   useEffect(() => {
-    const tween = KUTE.fromTo('#circle', {path:'#circle'}, {path: '#rectangle'}, {morphPrecision: 6, easing: 'easingQuadraticInOut'});
-    setTimeout(() => tween.start(), 1500);
+    //const tween = KUTE.fromTo('#circle', {path:'#circle'}, {path: '#rectangle'}, {morphPrecision: 6, easing: 'easingQuadraticInOut'});
+    //setTimeout(() => tween.start(), 1500);
+
+    const gp = new GradientPath({
+      path: document.querySelector('#vector1'),
+      segments: 600,
+      samples: 5,
+      precision: 4 // Optional
+    });
+    
+    gp.render({
+      type: 'path',
+      stroke: [
+        { color: '#C6FFDD', pos: 0 },
+        { color: '#FBD786', pos: 0.25 },
+        { color: '#F7797D', pos: 0.5 },
+        { color: '#6DD5ED', pos: 0.75 },
+        { color: '#C6FFDD', pos: 1 }
+      ],
+      fill: [
+        { color: '#C6FFDD', pos: 0 },
+        { color: '#FBD786', pos: 0.25 },
+        { color: '#F7797D', pos: 0.5 },
+        { color: '#6DD5ED', pos: 0.75 },
+        { color: '#C6FFDD', pos: 1 }
+      ],
+      width: 0,
+      strokeWidth: 3,
+    });
+    
+
   }, []);
   return (<div className={TitleStyle.Title}>
-    <TitleSVG />
+    <MorphingCircleSVG id={TitleStyle.morphingCircle} />
+    <DominicSVG id={TitleStyle.Dominic} />
   </div>);
 }
 export default Title;
