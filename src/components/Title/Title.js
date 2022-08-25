bf6eacab11a26da55f9e791240704ba9ac6135a9
6okuJins <d.ma@mail.utoronto.ca>
2022-08-24 21:01:53 -0400

    Finish name animation

--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -6,35 +6,10 @@ import {GradientPath} from 'gradient-path';
 
 const Title = () => {
   useEffect(() => {
-    //const tween = KUTE.fromTo('#circle', {path:'#circle'}, {path: '#rectangle'}, {morphPrecision: 6, easing: 'easingQuadraticInOut'});
-    //setTimeout(() => tween.start(), 1500);
+    const tween = KUTE.fromTo('#circle', {path:'#circle'}, {path: '#rectangle'}, {morphPrecision: 6, easing: 'easingQuadraticInOut'});
+    setTimeout(() => tween.start(), 1500);
 
-    const gp = new GradientPath({
-      path: document.querySelector('#vector1'),
-      segments: 600,
-      samples: 5,
-      precision: 4 // Optional
-    });
-    
-    gp.render({
-      type: 'path',
-      stroke: [
-        { color: '#C6FFDD', pos: 0 },
-        { color: '#FBD786', pos: 0.25 },
-        { color: '#F7797D', pos: 0.5 },
-        { color: '#6DD5ED', pos: 0.75 },
-        { color: '#C6FFDD', pos: 1 }
-      ],
-      fill: [
-        { color: '#C6FFDD', pos: 0 },
-        { color: '#FBD786', pos: 0.25 },
-        { color: '#F7797D', pos: 0.5 },
-        { color: '#6DD5ED', pos: 0.75 },
-        { color: '#C6FFDD', pos: 1 }
-      ],
-      width: 0,
-      strokeWidth: 3,
-    });
+
     
 
   }, []);
